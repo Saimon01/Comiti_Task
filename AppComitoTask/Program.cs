752bc34dfc556038293daafd4bcb0ca789e672dd
Simone Zugarini <zuga_simo@hotmail.it>
2018-09-26 17:17:06 +0200

    Creazione metodo Write e inserimento del Task nel progetto

---
+++ AppComitoTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppComitoTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Console.Write("Ciao");
+            Task t = new Task(Write);
+            t.Start();
+            int i = 100;
+            while (i > 0)
+            {
+                Console.Write("Hello");
+                i = i + 1;
+            }
+            Console.WriteLine();
+        }
+        public static void Write()
+        {
+            int i = 10;
+            while (i >= 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+        }
+    }
+}
